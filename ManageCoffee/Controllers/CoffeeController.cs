a582a3f58e4ff44f512dc386fde839394a0962ce
Văn Quyết <phamhoangh7@gmail.com>
2023-12-26 01:33:54 +0700

    update-function Login

--- ManageCoffee/Controllers/CoffeeController.cs
+++ ManageCoffee/Controllers/CoffeeController.cs
@@ -1,25 +1,26 @@
-﻿using ManageCoffee.Models;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using ManageCoffee.Models;
 using ManageCoffee.Other;
-using ManageCoffee.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using System.Globalization;
 using System.Linq;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ManageCoffee.Controllers
 {
     public class CoffeeController : Controller
     {
         private readonly CoffeeShopContext _context;
-
-        public CoffeeController(CoffeeShopContext context)
+        private readonly INotyfService _notyfService;
+        public CoffeeController(INotyfService notyfService, CoffeeShopContext context)
         {
             _context = context;
-
+            _notyfService = notyfService;
         }
-
+        //Hiển thị danh sách coffee
         [HttpGet]
         public async Task<IActionResult> DanhSachCoffee(string searchString, string currentFilter, int? pageNumber)
         {
@@ -50,15 +51,130 @@ namespace ManageCoffee.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult ThemCoffee([FromBody] Coffee coffee)
+        public async Task<IActionResult> ThemCoffee([Bind("Id", "Name", "Description", "Type", "Image", "Price", "Quantity", "InStock")] Coffee coffee, IFormFile formFile)
         {
             if (ModelState.IsValid)
             {
-                _context.Coffee.Add(coffee);
-                _context.SaveChanges();
+                string filename = formFile.FileName;
+                coffee.Image = filename.ToString(); // tên ảnh
+                var imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/assets/img", filename);
+                formFile.CopyTo(new FileStream(imagePath, FileMode.Create));
+                _context.Add(coffee);
+                await _context.SaveChangesAsync();
+                _notyfService.Success("Bạn đã thêm thông tin của sản phẩm thành công.");
+                return RedirectToAction(nameof(DanhSachCoffeeAdmin));
             }
-            return RedirectToAction("DanhSachCoffee", "Coffee");
+            else
+            {
+                _notyfService.Error("Vui lòng nhập lại thông tin của sản phẩm");
+            }
+            return View(coffee);
         }
-       
+        public async Task<IActionResult> CapNhatCoffee(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coffee = await _context.Coffee.FindAsync(id);
+            if (coffee == null)
+            {
+                return NotFound();
+            }
+            return View(coffee);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CapNhatCoffee(int id, [Bind("Id,Name,Description,Type,Image,Price,Quantity,InStock")] Coffee coffee)
+        {
+            if (id != coffee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(coffee);
+                    await _context.SaveChangesAsync();
+                    _notyfService.Success("Bạn đã cập nhật thông tin coffee thành công.");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoffeeExists(coffee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(DanhSachCoffee));
+            }
+
+
+            return View(coffee);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coffee = await _context.Coffee
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (coffee == null)
+            {
+                return NotFound();
+            }
+
+            return View(coffee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+
+            var coffee = await _context.Coffee.FindAsync(id);
+            _context.Coffee.Remove(coffee);
+            await _context.SaveChangesAsync();
+            _notyfService.Success("Bạn đã xóa thông tin của coffee thành công.");
+            return RedirectToAction(nameof(DanhSachCoffee));
+        }
+        private bool CoffeeExists(int id)
+        {
+            return _context.Coffee.Any(e => e.Id == id);
+        }
+        [HttpGet]
+        public async Task<IActionResult> DanhSachCoffeeAdmin(string searchString, string currentFilter, int? pageNumber)
+        {
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            var sqlServerDbContext = from s in _context.Coffee
+                                     select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sqlServerDbContext = sqlServerDbContext.Where(s => s.Name.Contains(searchString));
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<Coffee>.CreateAsync(sqlServerDbContext.AsNoTracking(), pageNumber ?? 1, pageSize));
+
+        }
     }
 }
